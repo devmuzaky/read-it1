cc9582184247c7655bec2a00a8f3dc7deca9368b
Muhammad Zaky <Devmuzaky@gmail.com>
2022-09-16 14:55:39 +0200

    from scratch until App

--- main.js
+++ main.js
@@ -1,34 +1,66 @@
-const { app, BrowserWindow } = require('electron')
-
+const {app, BrowserWindow} = require('electron')
 
 let mainWindow
 
+// setTimeout(() => {
+//     console.log(app.isReady())
+// }, 2000)
 
-function createWindow () {
+
+function createWindow() {
     mainWindow = new BrowserWindow(
         {
-            width:1000,
-            height:800,
-            webPreferences: {nodeIntegration: true}
+            width: 800,
+            height: 600,
+            webPreferences: {nodeIntegration: true},
+            show: false
         }
     )
+    mainWindow.loadFile('index.html')
+    // mainWindow.loadURL('https://google.com')
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.once('ready-to-show', mainWindow.show)
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 }
+//
+// app.on('before-quit', event => {
+//
+//     console.log('Preventing quit')
+//     event.preventDefault()
+// })
 
-mainWindow.loadFile('index.html')
 
 
 
 
 
-app.whenReady().then(createWindow)
+// app.on('browser-window-blur', e => {
+//     console.log('App Unfocused')
+//     setTimeout(app.quit, 3000)
+// })
+
+// app.on('browser-window-focus', e => {
+//     console.log('App Focused')
+// })
+
+app.on('ready', () =>{
+    createWindow()
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
+
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
+    if (mainWindow === null) {
         createWindow()
     }
 })
+
